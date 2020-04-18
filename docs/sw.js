0ff19a4689b27eb706b358146ba8fca1ea01f604
Andrey Lushnikov <lushnikov@chromium.org>
2020-04-17 18:39:43 -0700

    chore: release 0.9.5+04c98ed

--- docs/sw.js
+++ docs/sw.js
@@ -20,7 +20,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
 
-workbox.precaching.precacheAndRoute([{"revision":"14a79243a0d5b16a0d1fa73ffec8ed37","url":"all.css"},{"revision":"8750ef8097f7a2ad51a1e74748bbb4b6","url":"index.html"},{"revision":"917c8e4b817e9ec769da012954bd74e0","url":"main.js"}]);
+workbox.precaching.precacheAndRoute([{"revision":"36aa3a5c0179614dcd9235d28894dcf1","url":"all.css"},{"revision":"8750ef8097f7a2ad51a1e74748bbb4b6","url":"index.html"},{"revision":"ce058103ae472c9797bf1e3182c5d9df","url":"main.js"}]);
 
 // This is needed to make SPA to work offline.
 // workbox.routing.registerNavigationRoute("index.html");
