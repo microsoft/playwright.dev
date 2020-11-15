4522ba461f8d89cff5797543926d9aa100cc6665
Arjun Attam <arjunattam@gmail.com>
2020-11-14 19:07:13 -0800

    feat: homepage and styles

--- v2/README.md
+++ v2/README.md
@@ -1,6 +1,6 @@
 # playwright.dev-v2
 
-This website is built using [Docusaurus v2](https://v2.docusaurus.io/). Content is pulled from the microsoft/playwright repo.
+This website is built using [Docusaurus v2](https://v2.docusaurus.io/). Content is pulled from the microsoft/playwright repo. [Preview link][preview].
 
 ## Development
 
@@ -34,4 +34,6 @@ npm run build
 
 ## Deployment
 
-See [GitHub Action](../.github/workflows/deploy-v2.yml) for deployment to [preview link](https://microsoft.github.io/playwright).
+See [GitHub Action](../.github/workflows/deploy-v2.yml) for deployment to [preview link][preview].
+
+[preview]: https://microsoft.github.io/playwright
--- v2/src/pages/index.js
+++ v2/src/pages/index.js
@@ -1,45 +1,40 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 const features = [
   {
-    title: 'Test across all modern browsers',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Single API to author browser tests for Chromium, Firefox and WebKit.
-      </>
-    ),
+    title: "Test across all modern browsers",
+    // imageUrl: "img/undraw_docusaurus_mountain.svg",
+    description: <>Single automation API for Chromium, Firefox and WebKit.</>,
+    link: "docs/why-playwright#support-for-all-browsers",
   },
   {
-    title: 'Use in your preferred language',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    title: "Automation without trade-offs",
+    // imageUrl: "img/undraw_docusaurus_react.svg",
     description: (
-      <>
-        Use the API in JavaScript/TypeScript, Python, C# and Java.
-      </>
+      <>Automation for single page apps that use the modern web platform.</>
     ),
+    link: "docs/why-playwright#powerful-automation-capabilities",
   },
   {
-    title: 'Automation without trade-offs',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
+    title: "Use in your preferred language",
+    // imageUrl: "img/undraw_docusaurus_tree.svg",
     description: (
-      <>
-        Playwright is built to automate the broad and growing set of web browser capabilities.
-      </>
+      <>Use the API in JavaScript/TypeScript, Python, C# and Java.</>
     ),
+    link: "docs/languages",
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description, link }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx('col col--4', styles.feature)}>
+    <div className={clsx("col col--4", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
@@ -47,30 +42,45 @@ function Feature({imageUrl, title, description}) {
       )}
       <h3>{title}</h3>
       <p>{description}</p>
+      <p>
+        <Link to={useBaseUrl("docs/why-playwright#support-for-all-browsers")}>
+          Learn more
+        </Link>
+      </p>
     </div>
   );
 }
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
-      title={siteConfig.title}
-      description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      title={siteConfig.tagline}
+      description="Cross-browser end-to-end testing for modern web apps"
+    >
+      <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <h1 className={clsx("hero__title", styles.heroTitle)}>
+            <span className={clsx(styles.highlight)}>Playwright</span> enables
+            reliable end-to-end testing for modern web apps.
+          </h1>
           <div className={styles.buttons}>
             <Link
-              className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/intro')}>
-              Get Started
+              className={clsx(styles.getStarted)}
+              to={useBaseUrl("docs/intro")}
+            >
+              Get started
             </Link>
+            <div>
+              <iframe
+                className={styles.githubStars}
+                src="https://ghbtns.com/github-btn.html?user=microsoft&amp;repo=playwright&amp;type=star&amp;count=true&amp;size=large"
+                width={160}
+                height={30}
+                title="GitHub Stars"
+              />
+            </div>
           </div>
         </div>
       </header>
