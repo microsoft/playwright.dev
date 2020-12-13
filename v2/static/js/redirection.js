0beab4940d8c7fe8602c38082da564b960fab2e6
Arjun Attam <arjunattam@gmail.com>
2020-12-13 20:38:35 +0530

    unregister sw

--- v2/static/js/redirection.js
+++ v2/static/js/redirection.js
@@ -1,18 +1,31 @@
 function param(name, url = window.location.href) {
-  name = name.replace(/[\[\]]/g, '\\$&');
-  var regex = new RegExp('[#&]' + name + '(=([^&#]*)|&|#|$)'),
-      results = regex.exec(url);
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[#&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
   if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-const path = param('path');
-const q = param('q');
-const version = param('version');
+const path = param("path");
+const q = param("q");
+const version = param("version");
 
 if (path) {
   // TODO: use versions
   // http://localhost:3000/#version=master&path=docs%2Femulation.md&q=geolocation
-  window.location.href = `${param('path').replace('.md', '')}#${param('q')}`;
+  window.location.href = `${param("path").replace(".md", "")}#${param("q")}`;
+}
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .getRegistrations()
+    .then(function (registrations) {
+      for (let registration of registrations) {
+        registration.unregister();
+      }
+    })
+    .catch(function (err) {
+      console.log("Service Worker registration failed: ", err);
+    });
 }
