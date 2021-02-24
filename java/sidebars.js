0e6c493077db7d512d03ad8191ce5136b12c503e
Yury Semikhatsky <yurys@chromium.org>
2021-02-23 17:29:39 -0800

    feat: initial java documentation (#125)

---
+++ java/sidebars.js
@@ -0,0 +1,95 @@
+module.exports = {
+  "docs": [
+    {
+      "type": "category",
+      "label": "Introduction",
+      "items": [
+        { "type": "doc", "id": "why-playwright" },
+        { "type": "doc", "id": "intro" },
+        { "type": "doc", "id": "core-concepts" },
+        { "type": "doc", "id": "debug" },
+        { "type": "doc", "id": "languages" }
+      ],
+      "collapsed": false
+    },
+    {
+      "type": "category",
+      "label": "Guides",
+      "items": [
+        { "type": "doc", "id": "actionability" },
+        { "type": "doc", "id": "assertions" },
+        { "type": "doc", "id": "auth" },
+        { "type": "doc", "id": "dialogs" },
+        { "type": "doc", "id": "downloads" },
+        { "type": "doc", "id": "selectors" },
+        { "type": "doc", "id": "emulation" },
+        { "type": "doc", "id": "extensibility" },
+        { "type": "doc", "id": "input" },
+        { "type": "doc", "id": "inspector" },
+        { "type": "doc", "id": "installation" },
+        { "type": "doc", "id": "multi-pages" },
+        { "type": "doc", "id": "navigations" },
+        { "type": "doc", "id": "network" },
+        { "type": "doc", "id": "pom" },
+        { "type": "doc", "id": "verification" },
+      ],
+      "collapsed": false
+    },
+    {
+      "type": "category",
+      "label": "Integrations",
+      "items": [
+        { "type": "doc", "id": "docker" },
+        { "type": "doc", "id": "ci" },
+        { "type": "doc", "id": "cli" }
+      ],
+      "collapsed": false
+    },
+    {
+      "type": "category",
+      "label": "Get help",
+      "items": [],
+      "collapsed": false
+    }
+  ],
+  "api": [
+    {
+      "type": "category",
+      "label": "API reference",
+      "items": [
+        { "type": "doc", "id": "api/class-playwright" },
+        {
+          "type": "category",
+          "label": "Classes",
+          "items": [
+            { "type": "doc", "id": "api/class-accessibility" },
+            { "type": "doc", "id": "api/class-browser" },
+            { "type": "doc", "id": "api/class-browsercontext" },
+            { "type": "doc", "id": "api/class-browsertype" },
+            { "type": "doc", "id": "api/class-consolemessage" },
+            { "type": "doc", "id": "api/class-dialog" },
+            { "type": "doc", "id": "api/class-download" },
+            { "type": "doc", "id": "api/class-elementhandle" },
+            { "type": "doc", "id": "api/class-filechooser" },
+            { "type": "doc", "id": "api/class-frame" },
+            { "type": "doc", "id": "api/class-jshandle" },
+            { "type": "doc", "id": "api/class-keyboard" },
+            { "type": "doc", "id": "api/class-mouse" },
+            { "type": "doc", "id": "api/class-page" },
+            { "type": "doc", "id": "api/class-request" },
+            { "type": "doc", "id": "api/class-response" },
+            { "type": "doc", "id": "api/class-route" },
+            { "type": "doc", "id": "api/class-selectors" },
+            { "type": "doc", "id": "api/class-touchscreen" },
+            { "type": "doc", "id": "api/class-video" },
+            { "type": "doc", "id": "api/class-websocket" },
+            { "type": "doc", "id": "api/class-websocketframe" },
+            { "type": "doc", "id": "api/class-worker" },
+          ],
+          "collapsed": false
+        },
+      ],
+      "collapsed": false
+    }
+  ]
+}
