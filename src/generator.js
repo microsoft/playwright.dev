1960e95beda82dcd1dff435d6ba3af4c60e8af11
Yury Semikhatsky <yurys@chromium.org>
2021-04-08 18:55:40 -0700

    feat: start generating C# docs (#173)

--- src/generator.js
+++ src/generator.js
@@ -233,7 +233,7 @@ ${md.render([spec[i]])}
    * @param {string} name 
    */
   generateDoc(name) {
-    if (name.includes('-js') || name.includes('-python') || name.includes('-java') || name.includes('sharp'))
+    if (name.includes('-js') || name.includes('-python') || name.includes('-java') || name.includes('-csharp'))
       if (!name.includes('-' + this.lang))
         return;
     const content = fs.readFileSync(path.join(DIR_SRC, name)).toString();
@@ -610,6 +610,87 @@ new Generator('java', path.join(__dirname, '..', 'java', 'docs'), {
       case 'Readable': return '[InputStream]';
       case 'Serializable': return '[Object]';
       case 'URL': return '[String]';
+    }
+    return `[${text}]`;
+  },
+});
+
+new Generator('csharp', path.join(__dirname, '..', 'csharp', 'docs'), {
+  formatMember: member => {
+    let text;
+    let args = [];
+    if (member.kind === 'property')
+      text = `${toTitleCase(member.clazz.varName)}.${toTitleCase(member.alias)}`;
+    if (member.kind === 'event')
+      text = `event ${toTitleCase(member.clazz.varName)}.${toTitleCase(member.alias)}`;
+    if (member.kind === 'method' ) {
+      args = member.argsArray;
+      const signature = renderJSSignature(args);
+      text = `${toTitleCase(member.clazz.varName)}.${toTitleCase(member.alias)}(${signature})`;
+    }
+    return { text, args };
+  },
+  formatArgumentName: name => name,
+  formatTemplate: text => `<${text}>`,
+  formatFunction: (args, ret, type) => {
+    if (type.args.length !== 1)
+      throw new Error('Unsupported number of arguments in function: ' + type);
+    if (!type.returnType)
+      return "[Action]<" + args + ">";
+    if (type.returnType.name === 'boolean')
+      return "[Func]<" + args + ", bool>";
+    throw new Error('Unknown C# type for function: ' + type);
+  },
+  formatPromise: text => text,
+  renderType: (type, direction, member) => {
+    if (member.kind === 'property' && member.name === 'options') {
+      const method = member.enclosingMethod;
+      return `\`${toTitleCase(method.clazz.varName)}.${toTitleCase(method.alias)}Options\``;
+    }
+    const text = type.name;
+    switch (text) {
+      case 'any': return '[object]';
+      case 'Array': return '[IEnumerable]';
+      case 'float': return '[double]';
+      case 'function': {
+        switch (fullName(member)) {
+          case 'BrowserContext.exposeBinding.callback': return '`Func<BindingSource, T, TResult>`';
+          case 'BrowserContext.exposeFunction.callback': return '`Func<T, TResult>`';
+          case 'Page.exposeBinding.callback': return '`Func<BindingSource, T, TResult>`';
+          case 'Page.exposeFunction.callback': return '`Func<T, TResult>`';
+        }
+        throw new Error('Unknwon C# type for ' + fullName(member));
+      };
+      case 'null': return '[null]';
+      case 'Object': {
+        // FIXME: generate correct type for accessibility snapshot children.
+        if (member.name === 'children') {
+          return '[IEnumerable]';
+        }
+        switch (fullName(member)) {
+          case 'BrowserContext.addCookies.cookies': return '`Cookie`';
+          case 'BrowserContext.cookies': return '`Cookie`';
+          case 'ElementHandle.selectOption.values': return '`SelectOption`';
+          case 'Frame.selectOption.values': return '`SelectOption`';
+          case 'Page.selectOption.values': return '`SelectOption`';
+          case 'ElementHandle.setInputFiles.files': return '`FilePayload`';
+          case 'FileChooser.setFiles.files': return '`FilePayload`';
+          case 'Frame.setInputFiles.files': return '`FilePayload`';
+          case 'Page.setInputFiles.files': return '`FilePayload`';
+        }
+        if (!type.templates)
+          return `\`${toTitleCase(member.alias)}\``;
+        return '[Map]';
+      }
+      case 'path': return '[string]';
+      case 'RegExp': return '[Regex]';
+      case 'string': return '[string]';
+      // Escape '[' and ']' so that they don't break markdown links like [byte[]](link)
+      case 'Buffer': return '[byte&#91;&#93;]';
+      case 'EvaluationArgument': return '[object]';
+      case 'Readable': return '[Stream]';
+      case 'Serializable': return '[object]';
+      case 'URL': return '[string]';
     }
     return `[${text}]`;
   },
