1e76032eaf405c797503d9d4322ec36ac3fc1b32
Max Schmitt <max@schmitt.mx>
2021-09-16 14:52:41 +0200

    fix it

--- dotnet/sidebars.js
+++ dotnet/sidebars.js
@@ -70,7 +70,6 @@ module.exports = {
             { "type": "doc", "id": "api/class-dialog" },
             { "type": "doc", "id": "api/class-download" },
             { "type": "doc", "id": "api/class-elementhandle" },
-            { "type": "doc", "id": "api/class-fetchresponse" },
             { "type": "doc", "id": "api/class-filechooser" },
             { "type": "doc", "id": "api/class-frame" },
             { "type": "doc", "id": "api/class-jshandle" },
