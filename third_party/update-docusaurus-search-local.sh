4a97f16b649dab82f3d72bd92572d3f6e748e0dd
Max Schmitt <max@schmitt.mx>
2021-11-30 20:59:41 +0100

    chore: bump dependencies (#431)

--- third_party/update-docusaurus-search-local.sh
+++ third_party/update-docusaurus-search-local.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-COMMIT_HASH="5c51edc1d46b2739236cd16b67e3f6e2ae7bd0cb"
+COMMIT_HASH="9093d71039f35409535ca77fdf2198d1d87291bf"
 
 cd third_party
 
@@ -10,7 +10,7 @@ TMP_ZIP_FILE="docusaurus-search-local.zip"
 
 rm -rf docusaurus-search-local/
 
-curl -o "${TMP_ZIP_FILE}" "https://codeload.github.com/easyops-cn/docusaurus-search-local/zip/${COMMIT_HASH}"
+curl --fail -o "${TMP_ZIP_FILE}" "https://codeload.github.com/easyops-cn/docusaurus-search-local/zip/${COMMIT_HASH}"
 
 unzip "${TMP_ZIP_FILE}"
 rm "${TMP_ZIP_FILE}"
