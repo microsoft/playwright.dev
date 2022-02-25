13d63fd0b9c1f8bb99b5cf542ee9f86ada8d6576
Max Schmitt <max@schmitt.mx>
2022-02-25 13:32:47 +0100

    chore: redirect away from /docs/assertions

--- static/js/redirection.js
+++ static/js/redirection.js
@@ -22,9 +22,17 @@ if (path) {
     { from: '/docs/installation', to: '/docs/library' },
     { from: '/docs/next/test-intro', to: '/docs/next/intro' },
     { from: '/docs/next/installation', to: '/docs/next/library' },
+    { from: '/docs/assertions', to: '/docs/test-assertions' },
+    { from: '/docs/next/assertions', to: '/next/docs/test-assertions' },
     // ------------------- python ---------------------
+    { from: '/python/docs/assertions', to: '/python/docs/api/class-playwrightassertions' },
+    { from: '/python/docs/next/assertions', to: '/python/next/docs/api/class-playwrightassertions' },
     // ------------------- dotnet ---------------------
+    { from: '/dotnet/docs/assertions', to: '/dotnet/docs/api/class-playwrightassertions' },
+    { from: '/dotnet/docs/next/assertions', to: '/dotnet/next/docs/api/class-playwrightassertions' },
     // ------------------- java -----------------------
+    { from: '/java/docs/assertions', to: '/java/docs/api/class-playwrightassertions' },
+    { from: '/java/docs/next/assertions', to: '/java/docs/next/api/class-playwrightassertions' },
   ];
   const pathname = window.location.pathname;
   for (const redirect of redirects) {
