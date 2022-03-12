923d16c714ef8b04a3ab2c25746e0a7c09e9f7d1
Max Schmitt <max@schmitt.mx>
2022-03-12 11:46:06 +0100

    chore: bump esbuild-loader (#513)

--- nodejs/docusaurus.config.js
+++ nodejs/docusaurus.config.js
@@ -189,7 +189,6 @@ module.exports = {
       loader: require.resolve('esbuild-loader'),
       options: {
         loader: 'tsx',
-        format: isServer ? 'cjs' : undefined,
         target: isServer ? 'node12' : 'es2017',
       },
     }),
