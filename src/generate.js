848248613152fff69a85b7eba13fe994c5526234
Max Schmitt <max@schmitt.mx>
2022-05-06 15:36:10 +0100

    chore: auto update stars button count

--- src/generate.js
+++ src/generate.js
@@ -16,14 +16,50 @@
 
 //@ts-check
 
+const fs = require('fs');
 const path = require('path');
+const https = require('https');
 const { Generator } = require('./generator');
 const { JavaScriptFormatter } = require('./format_js');
 const { PythonFormatter } = require('./format_python');
 const { JavaFormatter } = require('./format_java');
 const { CSharpFormatter } = require('./format_csharp');
 
-new Generator('js', path.join(__dirname, '..', 'nodejs', 'docs'), new JavaScriptFormatter());
-new Generator('python', path.join(__dirname, '..', 'python', 'docs'), new PythonFormatter());
-new Generator('java', path.join(__dirname, '..', 'java', 'docs'), new JavaFormatter());
-new Generator('csharp', path.join(__dirname, '..', 'dotnet', 'docs'), new CSharpFormatter());
+(async () => {
+  new Generator('js', path.join(__dirname, '..', 'nodejs', 'docs'), new JavaScriptFormatter());
+  new Generator('python', path.join(__dirname, '..', 'python', 'docs'), new PythonFormatter());
+  new Generator('java', path.join(__dirname, '..', 'java', 'docs'), new JavaFormatter());
+  new Generator('csharp', path.join(__dirname, '..', 'dotnet', 'docs'), new CSharpFormatter());
+
+  await updateStarsButton();
+})().catch(error => {
+  console.error(error);
+  process.exit(error);
+});
+
+async function updateStarsButton() {
+  const kGitHubStarsButtonSource = path.join(__dirname, 'components/GitHubStarButton/index.tsx');
+  const repoInfoResponse = await new Promise((resolve, reject) => {
+    https.get('https://api.github.com/repos/microsoft/playwright', {
+      headers: {
+        'User-Agent': 'playwright-docs-generator',
+      },
+    }, res => {
+      let data = '';
+      res.on('data', chunk => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        if (res.statusCode >= 200 && res.statusCode < 300)
+          resolve(JSON.parse(data));
+        else
+          reject(new Error(`Request failed with status code ${res.statusCode}: ${data}`));
+      })
+      res.on('error', (error) => reject(error))
+    });
+  });
+  const roundedStarsCount = Math.floor(repoInfoResponse.stargazers_count / 1000);
+  let content = (await fs.promises.readFile(kGitHubStarsButtonSource, 'utf8')).toString();
+  content = content.replace(/(STARS = ')(\d+)(k\+';)/, `$1${roundedStarsCount}$3`);
+  await fs.promises.writeFile(kGitHubStarsButtonSource, content);
+}
