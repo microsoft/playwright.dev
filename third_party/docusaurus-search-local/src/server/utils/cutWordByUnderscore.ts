6e8adc37f56a35eecdaa9d1c9f707bce7cfc23ac
Max Schmitt <max@schmitt.mx>
2022-06-22 15:16:00 +0200

    chore: remove docusaurus-search-local

--- third_party/docusaurus-search-local/src/server/utils/cutWordByUnderscore.ts
+++
@@ -1,7 +0,0 @@
-const splitRegExp = [/(_)([^_])/g, /([^_])(_)/g];
-
-export function cutWordByUnderscore(input: string): string[] {
-  return splitRegExp
-    .reduce((carry, re) => carry.replace(re, "$1\0$2"), input)
-    .split("\0");
-}
