ad2c1dec1343a3d64d6336ccd3b357fb2e1b2d48
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2022-07-28 17:17:52 +0200

    docs: improve responsiveness for ambassadors (#689)

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -54,103 +54,98 @@ function ProfileCard({
   );
 }
 
-function ProfileCardCol(props: ProfileProps) {
-  return <ProfileCard {...props} className='col col--3 margin-bottom--lg' />;
-}
 
 export function Team(): JSX.Element {
   return (
-    <div className='row'>
-      <ProfileCardCol
+    <div className={styles.cards}>
+      <ProfileCard
         name="Debbie O'Brien"
         city='Palma de Mallorca'
         country='Spain'
         githubUrl='https://github.com/debs-obrien'
         twitterUrl='https://twitter.com/debs_obrien'
         linkedInUrl='https://www.linkedin.com/in/debbie-o-brien-1a199975/'
-      ></ProfileCardCol>
-      <ProfileCardCol
+      ></ProfileCard>
+      <ProfileCard
         name='Max Schmitt'
         city='Berlin'
         country='Germany'
         githubUrl='https://github.com/mxschmitt'
         twitterUrl='https://twitter.com/maxibanki'
         linkedInUrl='https://www.linkedin.com/in/max-schmitt/'
-      ></ProfileCardCol>
-      <ProfileCardCol
+      ></ProfileCard>
+      <ProfileCard
         name='Ross Wollman'
         city='San Francisco'
         country='USA'
         githubUrl='https://github.com/rwoll'
         twitterUrl='https://twitter.com/rwoll_dev'
         linkedInUrl='https://www.linkedin.com/in/rwoll/'
-      ></ProfileCardCol>
+      ></ProfileCard>
     </div>
   );
 }
 
 export function Ambassadors(): JSX.Element {
   return (
-    <div className='row'>
-      <ProfileCardCol
+    <div className={styles.cards}>
+      <ProfileCard
         name='Andrew Knight'
         city='North Carolina'
         country='USA'
         githubUrl='https://github.com/AutomationPanda'
         twitterUrl='https://twitter.com/AutomationPanda'
-      ></ProfileCardCol>
-      <ProfileCardCol
+      ></ProfileCard>
+      <ProfileCard
         name='Carlos Gauto'
         city='Berazategui'
         country='Argentina'
         githubUrl='https://github.com/charlyautomatiza'
         twitterUrl='https://twitter.com/char_automatiza'
         websiteUrl='https://linktr.ee/charlyautomatiza'
-      ></ProfileCardCol>
-      <ProfileCardCol
+      ></ProfileCard>
+      <ProfileCard
         name='Giovanni Rago'
         city='Berlin'
         country='Germany'
         imageUrl='https://media-exp1.licdn.com/dms/image/C4E03AQHNkQmEbZBVqA/profile-displayphoto-shrink_400_400/0/1517375686271?e=1664409600&v=beta&t=r7M2zqvKeA6Ogd_mHbq2-0YFZfNGGVBQtZK3b9tpFEw'
         twitterUrl='https://twitter.com/rag0g'
         websiteUrl='https://www.youtube.com/c/AutomateTogether'
-      ></ProfileCardCol>
-      <ProfileCardCol
+      ></ProfileCard>
+      <ProfileCard
         name='John Hill'
         city='Palo Alto'
         country='USA'
         githubUrl='https://github.com/unlikelyzero'
-      ></ProfileCardCol>
-      <ProfileCardCol
+      ></ProfileCard>
+      <ProfileCard
         name='Katrik K. K'
         city='Auckland'
         country='New Zealand'
         imageUrl='https://media-exp1.licdn.com/dms/image/C5603AQFvGgrfen7F9Q/profile-displayphoto-shrink_400_400/0/1629605742823?e=1664409600&v=beta&t=yZx1w3KMdPxznERLrH1Rg7cRlNVuomVHzklnR9cMcVU'
         linkedInUrl='https://www.linkedin.com/in/karthikkk/'
-      ></ProfileCardCol>
-      
-
-      <ProfileCardCol
+      ></ProfileCard>
+      <ProfileCard
         name='Stefan Judis'
         city='Berlin'
         country='Germany'
         githubUrl='https://github.com/stefanjudis'
         twitterUrl='https://twitter.com/stefanjudis'
         websiteUrl='https://www.stefanjudis.com'
-      ></ProfileCardCol>
-      <ProfileCardCol
+      ></ProfileCard>
+      <ProfileCard
         name='Tally Barak'
         city='Tel Aviv'
         country='Israel'
         githubUrl='https://github.com/Tallyb'
         twitterUrl='https://twitter.com/TallyBarak'
-      ></ProfileCardCol>
-      <ProfileCardCol
+      ></ProfileCard>
+      <ProfileCard
         name='Are you the next Ambassador?'
         city=''
         country=''
         imageUrl='https://playwright.dev/img/playwright-logo.svg'
-      ></ProfileCardCol>
+      ></ProfileCard>
     </div>
   );
 }
