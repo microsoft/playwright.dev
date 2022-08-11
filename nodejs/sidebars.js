ffe997c0f1144f8c476de0114e587e10dc2a23d6
Max Schmitt <max@schmitt.mx>
2022-08-11 12:31:29 +0200

    docs: add trace viewer intro to sidebars (#727)

--- nodejs/sidebars.js
+++ nodejs/sidebars.js
@@ -8,7 +8,7 @@ module.exports = {
         { type: 'doc', id: 'writing-tests' },
         { type: 'doc', id: 'running-tests' },
         { type: 'doc', id: 'codegen-intro' },
-        { type: 'doc', id: 'trace-viewer' },
+        { type: 'doc', id: 'trace-viewer-intro' },
         { type: 'doc', id: 'ci-intro' },
       ],
       collapsed: false
@@ -72,6 +72,7 @@ module.exports = {
         { type: 'doc', id: 'screenshots' },
         { type: 'doc', id: 'selectors' },
         { type: 'doc', id: 'codegen' },
+        { type: 'doc', id: 'trace-viewer' },
         { type: 'doc', id: 'videos' }
       ],
       collapsed: false
