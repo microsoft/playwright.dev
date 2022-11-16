c6f0d043c2bf30c972852c5b79e68a5c6dc3fd24
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2022-11-16 17:02:10 +0100

    chore: move components  and add copyright

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -15,22 +15,14 @@
  */
 
 import React from 'react';
-import Layout from '@theme/Layout';
-import VideoCards from '@site/src/components/VideoCards';
-import PageHeader from '@site/src/components/PageHeader';
 
-import featureVideos from '@site/src/data/feature-videos';
-
-const TITLE = 'Feature Videos';
-const DESCRIPTION = 'Check out the latest Playwright feature videos';
-
-export default function Video(): JSX.Element {
+function PageHeader({ title, description }) {
   return (
-    <Layout title={TITLE} description={DESCRIPTION}>
-      <main className="margin-vert--lg">
-        <PageHeader title={TITLE} description={DESCRIPTION} />
-        <VideoCards videos={featureVideos} />
-      </main>
-    </Layout>
+    <section className="margin-top--lg margin-bottom--lg text--center">
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </section>
   );
 }
+
+export default PageHeader;
