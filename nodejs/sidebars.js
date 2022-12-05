242cf6b5e860c45fb77a02e41d6a301314df3fc1
Max Schmitt <max@schmitt.mx>
2022-12-05 08:41:23 -0800

    chore: remove selectors.md from sidebar

--- nodejs/sidebars.js
+++ nodejs/sidebars.js
@@ -69,7 +69,6 @@ module.exports = {
         { type: 'doc', id: 'pom' },
         { type: 'doc', id: 'pages' },
         { type: 'doc', id: 'screenshots' },
-        { type: 'doc', id: 'selectors' },
         { type: 'doc', id: 'codegen' },
         { type: 'doc', id: 'trace-viewer' },
         { type: 'doc', id: 'videos' },
