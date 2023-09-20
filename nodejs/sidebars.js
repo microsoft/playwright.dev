4ea9c1bbaf3dd62c8edfe4dd71719fcd17c2aab7
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2023-09-20 15:36:22 +0200

    docs: update sidebar (#1160)

--- nodejs/sidebars.js
+++ nodejs/sidebars.js
@@ -6,10 +6,9 @@ module.exports = {
       items: [
         { type: 'doc', id: 'intro' },
         { type: 'doc', id: 'writing-tests' },
-        { type: 'doc', id: 'running-tests' },
         { type: 'doc', id: 'codegen-intro' },
+        { type: 'doc', id: 'running-tests' },
         { type: 'doc', id: 'trace-viewer-intro' },
-        { type: 'doc', id: 'test-ui-mode' },
         { type: 'doc', id: 'ci-intro' },
       ],
       collapsed: false
