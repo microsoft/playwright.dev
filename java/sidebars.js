aa91df3e3e9dfb2f587413969d92a0dc3db7b6e0
Yury Semikhatsky <yurys@chromium.org>
2023-09-21 10:47:32 -0700

    docs: add PlaywrightAssertions to the API sidebar

--- java/sidebars.js
+++ java/sidebars.js
@@ -117,7 +117,8 @@ module.exports = {
           items: [
             { type: 'doc', id: 'api/class-apiresponseassertions' },
             { type: 'doc', id: 'api/class-locatorassertions' },
-            { type: 'doc', id: 'api/class-pageassertions' }
+            { type: 'doc', id: 'api/class-pageassertions' },
+            { type: 'doc', id: 'api/class-playwrightassertions' }
           ],
           collapsed: false
         }
