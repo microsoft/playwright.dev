04c1f1ea9cec8b5bb689b69ff725eeb5cc530da3
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2024-05-29 14:39:24 +0200

    community: add new video and improve titles

--- src/data/learn-videos.tsx
+++ src/data/learn-videos.tsx
@@ -1,40 +1,46 @@
 const learnVideos = [
   {
-    title: 'Introduction',
+    title: 'How to Run Tests in Playwright with the VS Code Extension',
     description: '',
-    id: '4-LwodVujTg',
+    id: 'mQmcIBMsc38',
+    year: '2024',
   },
   {
-    title: 'Getting Started',
-    description: '',
-    id: 'JdMkZUePkSE',
-  },
-  {
-    title: 'Running Tests',
-    description: '',
-    id: 'DcVYaZ8QDsw',
-  },
-  {
-    title: 'Writing Tests',
-    description: '',
-    id: 'GKG9g_JGZPc',
-  },
-  {
-    title: 'Debugging Tests',
-    description: '',
-    id: 'oYCSzYG-Bbg',
-  },
-  {
-    title: 'Running Tests on CI',
-    description: '',
-    id: 'gRXPp6RuExU',
-  },
-  {
-    title: 'Generating Tests',
+    title: 'How to Generate Tests in playwright with the VS Code Extension',
     description: '',
     id: '5XIZPqKkdBA',
     year: '2024',
   },
+  {
+    title: 'Get Started with end-to-end testing: Playwright. Introduction',
+    description: '',
+    id: '4-LwodVujTg',
+  },
+  {
+    title: 'Get Started with end-to-end testing: Playwright. Getting Started',
+    description: '',
+    id: 'JdMkZUePkSE',
+  },
+  {
+    title: 'Get Started with end-to-end testing: Playwright. Running Tests',
+    description: '',
+    id: 'DcVYaZ8QDsw',
+  },
+  {
+    title: 'Get Started with end-to-end testing: Playwright. Writing Tests',
+    description: '',
+    id: 'GKG9g_JGZPc',
+  },
+  {
+    title: 'Get Started with end-to-end testing: Playwright. Debugging Tests',
+    description: '',
+    id: 'oYCSzYG-Bbg',
+  },
+  {
+    title: 'Get Started with end-to-end testing: Playwright. Running Tests on CI',
+    description: '',
+    id: 'gRXPp6RuExU',
+  }
 ];
 
 export default learnVideos;
