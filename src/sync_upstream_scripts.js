92c82c3592240f5aefc81960fcf2ea2691c37247
Dmitry Gozman <dgozman@gmail.com>
2024-09-24 10:07:24 +0100

    chore: roll documentation scripts

--- src/sync_upstream_scripts.js
+++ src/sync_upstream_scripts.js
@@ -1,14 +1,13 @@
-const path = require("path")
-const fs = require("fs")
+const path = require('path');
+const fs = require('fs');
 
-const srcDir = process.env.SRC_DIR || '../playwright'
+const srcDir = process.env.SRC_DIR || '../playwright';
+fs.copyFileSync(path.join(srcDir, 'utils', 'markdown.js'), path.join(__dirname, 'markdown.js'));
 
-fs.copyFileSync(path.join(srcDir, "utils", "markdown.js"), path.join(__dirname, 'markdown.js'))
+let documentationJsFile = fs.readFileSync(path.join(srcDir, 'utils', 'doclint', 'documentation.js')).toString();
+documentationJsFile = documentationJsFile.replace(/\.\.\/markdown/g, './markdown');
+fs.writeFileSync(path.join(__dirname, 'documentation.js'), documentationJsFile);
 
-let documentationJsFile = fs.readFileSync(path.join(srcDir, "utils", "doclint", "documentation.js")).toString()
-documentationJsFile = documentationJsFile.replace(/\.\.\/markdown/g, './markdown')
-fs.writeFileSync(path.join(__dirname, 'documentation.js'), documentationJsFile)
-
-let apiParserJsFile = fs.readFileSync(path.join(srcDir, "utils", "doclint", "api_parser.js")).toString()
-apiParserJsFile = apiParserJsFile.replace(/\.\.\/markdown/g, './markdown')
-fs.writeFileSync(path.join(__dirname, 'api_parser.js'), apiParserJsFile)
+let apiParserJsFile = fs.readFileSync(path.join(srcDir, 'utils', 'doclint', 'api_parser.js')).toString();
+apiParserJsFile = apiParserJsFile.replace(/\.\.\/markdown/g, './markdown');
+fs.writeFileSync(path.join(__dirname, 'api_parser.js'), apiParserJsFile);
