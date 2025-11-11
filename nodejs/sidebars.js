3936089894a05a57cfa3cbd9187ec3d28a26049e
Simon Knott <info@simonknott.de>
2025-11-11 12:50:20 +0100

    and another one

--- nodejs/sidebars.js
+++ nodejs/sidebars.js
@@ -46,6 +46,7 @@ module.exports = {
       label: 'Guides',
       items: [
         { type: 'doc', id: 'library' },
+        { type: 'doc', id: 'accessibility-testing' },
         { type: 'doc', id: 'input' },
         { type: 'doc', id: 'test-assertions' },
         { type: 'doc', id: 'api-testing' },
