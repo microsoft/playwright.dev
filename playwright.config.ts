e8b93b8b6896aa7ad7ff96d82b1867787f2d1593
Pavel Feldman <pavel.feldman@gmail.com>
2026-09-04 10:30:03 -0700

    test: remove a11y tests (#2133)
    Removes the axe-based accessibility test suite and the Azure pipeline
    that existed solely to run it.

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ const config = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [['html'], ['junit', { outputFile: './test-results/junit.xml'}]] : [['html']],
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
